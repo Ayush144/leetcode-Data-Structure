47f8037b3867fe7a2ef25436c95dc4dd01901c0f
Ayush Tiwari <55916664+Ayush144@users.noreply.github.com>
2022-08-10 22:20:41 +0530

    Create Binary Tree Preorder Traversal.java

---
+++ Data Structure 1/DAY 10/Binary Tree Preorder Traversal.java
@@ -0,0 +1,44 @@
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+=========================================================================================================================================================================
+JAVA SOLUTION
+
+class Solution {
+    List<Integer> list=new ArrayList<Integer>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+    preorder(root);
+        return list;
+        
+    }
+    public  void preorder(TreeNode root){
+        if(root==null){
+            return ;
+        }
+        list.add(root.val);
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
